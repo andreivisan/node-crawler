ab7d5977189ccfdf2d766c20c2a7622d4c6456ca
Andrei Visan <andrei.m.visan@gmail.com>
2015-12-10 22:51:18 +0100

    Partial commit

---
+++ crawler/index.js
@@ -0,0 +1,37 @@
+var Crawler = require('simplecrawler');
+
+module.exports.crawl = function(crawlerCallback) {
+  var myCrawler = Crawler.crawl("http://programminglife.io/");
+
+  var urls = [];
+
+  myCrawler.maxDepth = 3;
+
+  myCrawler.on("crawlstart", function() {
+    console.log("Crawler started!");
+  });
+
+  // myCrawler.on("queueadd", function(queueItem) {
+  //   console.log("New url added to the queue: %s", JSON.stringify(queueItem));
+  // });
+  //
+  // myCrawler.on("queueerror", function(errorData, URLData) {
+  //   console.log("Item %s could not be added to the queue because: %s", URLData, errorData);
+  // });
+  //
+  // myCrawler.on("fetcherror", function(queueItem, response) {
+  //   console.log("Information could not be fetched from %s due to error", queueItem);
+  // });
+
+  myCrawler.on("fetchcomplete", function(queueItem, responseBuffer, response) {
+      console.log("I just received %s (%d bytes)", queueItem.url, responseBuffer.length);
+      urls.push(queueItem);
+  });
+
+  myCrawler.on("complete", function() {
+    console.log("Complete with %d items", urls.length);
+    crawlerCallback(" ");
+  });
+
+  myCrawler.start();
+}
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+var crawler = require('../crawler/index');
+
 /* GET home page. */
 router.get('/', function(req, res) {
+  crawler.crawl();
   res.render('index', { title: 'Express' });
 });
 
