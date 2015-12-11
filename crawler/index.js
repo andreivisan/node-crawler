98c69e635ab513808387747dfb3ace5fa5e9eadb
Andrei Visan <andrei.m.visan@gmail.com>
2015-12-11 22:49:45 +0100

    Partial commit

--- crawler/index.js
+++ crawler/index.js
@@ -1,27 +1,25 @@
 var Crawler = require('simplecrawler');
 
-module.exports.crawl = function() {
+module.exports.crawl = function(callback) {
   var myCrawler = Crawler.crawl("http://programminglife.io/");
 
   var urls = [];
-
-  myCrawler.maxDepth = 3;
 
   myCrawler.on("crawlstart", function() {
     console.log("Crawler started!");
   });
 
-  // myCrawler.on("queueadd", function(queueItem) {
-  //   console.log("New url added to the queue: %s", JSON.stringify(queueItem));
-  // });
-  //
-  // myCrawler.on("queueerror", function(errorData, URLData) {
-  //   console.log("Item %s could not be added to the queue because: %s", URLData, errorData);
-  // });
-  //
-  // myCrawler.on("fetcherror", function(queueItem, response) {
-  //   console.log("Information could not be fetched from %s due to error", queueItem);
-  // });
+  myCrawler.on("queueadd", function(queueItem) {
+    console.log("New url added to the queue: %s", queueItem.url);
+  });
+
+  myCrawler.on("queueerror", function(errorData, URLData) {
+    console.log("Item %s could not be added to the queue because: %s", URLData, errorData);
+  });
+
+  myCrawler.on("fetcherror", function(queueItem, response) {
+    console.log("Information could not be fetched from %s due to error", queueItem.url);
+  });
 
   myCrawler.on("fetchcomplete", function(queueItem, responseBuffer, response) {
       console.log("I just received %s (%d bytes)", queueItem.url, responseBuffer.length);
